e4f48def19d52b8fb12cb9e629b61bc41c1db1ef
Kragline <armanaraqelyan1107@gmail.com>
2023-12-17 19:33:09 -0800

    fixed some bugs

--- sorter.py
+++ sorter.py
@@ -9,7 +9,7 @@ from tkinter import messagebox
 
 class FileSorter:
     def __init__(self, master: Tk):
-        self.files_by_type = self.load_extensions()
+        self.files_by_type = self.__load_extensions()
         self.directory = None
 
         self.window = master
@@ -20,13 +20,13 @@ class FileSorter:
         self.input = ttk.Entry(self.window, width=30)
         self.input.grid(row=0, column=1)
 
-        self.browse_button = ttk.Button(self.window, text='Browse', command=self.browse_directory)
+        self.browse_button = ttk.Button(self.window, text='Browse', command=self.__browse_directory)
         self.browse_button.grid(row=0, column=2, padx=10, pady=10)
 
-        self.sort_button = ttk.Button(self.window, text='Sort', command=self.sort_files)
+        self.sort_button = ttk.Button(self.window, text='Sort', command=self.__sort_files)
         self.sort_button.grid(row=1, column=2, padx=10, pady=10)
 
-        self.clear_button = ttk.Button(self.window, text='Clear', command=self.clear)
+        self.clear_button = ttk.Button(self.window, text='Clear', command=self.__clear)
         self.clear_button.grid(row=1, column=1, padx=(110, 0), pady=10)
 
     @classmethod
@@ -51,32 +51,36 @@ class FileSorter:
         shutil.move(str(src_path), str(os.path.join(target_dir, os.path.basename(src_path))))
 
     def __browse_directory(self):
-        self.directory = filedialog.askdirectory()
-        self.input.insert(0, self.directory)
+        if new_directory := filedialog.askdirectory():
+            self.input.delete(0, END)
+            self.input.insert(0, new_directory)
 
     def __get_dir_path(self):
         if dir_path := self.input.get():
             self.directory = dir_path
 
     def __sort_files(self):
-        self.get_dir_path()
+        self.__get_dir_path()
 
         if self.directory:
-            for file in os.listdir(self.directory):
-                file_path = os.path.join(self.directory, file)
-                if os.path.isfile(file_path):
-                    f_extension = os.path.splitext(file_path)[1][1:]
-                    file_type = self.get_file_type(f_extension)
+            try:
+                for file in os.listdir(self.directory):
+                    file_path = os.path.join(self.directory, file)
+                    if os.path.isfile(file_path):
+                        f_extension = os.path.splitext(file_path)[1][1:]
+                        file_type = self.__get_file_type(f_extension)
 
-                    if file_type == 'other':
-                        target_path = os.path.join(self.directory, 'Other files')
-                    else:
-                        target_path = os.path.join(self.directory, file_type.capitalize() + ' files')
+                        if file_type == 'other':
+                            target_path = os.path.join(self.directory, 'Other files')
+                        else:
+                            target_path = os.path.join(self.directory, file_type.capitalize() + ' files')
 
-                    self.move_file(file_path, target_path)
-            self.clear()
-            os.startfile(self.directory)
-            self.directory = None
+                        self.__move_file(file_path, target_path)
+                self.__clear()
+                os.startfile(self.directory)
+                self.directory = None
+            except FileNotFoundError:
+                messagebox.showerror("Directory error", "No such directory")
         else:
             messagebox.showwarning('Warning!', 'Specify directory for sorting')
 
